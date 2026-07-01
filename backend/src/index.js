f835226b373879abc05aac7db70ef9b8ca2f653c
ItsLok0 <pierre.parent2020@gmail.com>
2026-07-01 14:13:25 +0200

    feat: add GET /links and GET /links/:id/stats endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const linkRoutes = require('./routes/link.routes')
-const authRoutes = require('./routes/auth.routes')
-const errorHandler = require('./middlewares/errorHandler')
-const { redirectLink } = require('./controllers/link.controller')
-
-
 // Parse le JSON des requêtes entrantes
 app.use(express.json())
 
+const linkRoutes = require('./routes/link.routes')
+const authRoutes = require('./routes/auth.routes')
 // Routes
 app.use('/links', linkRoutes)
 app.use('/auth', authRoutes)
 
+const { redirectLink } = require('./controllers/link.controller')
 // Toujours après les routes (redirect)
 app.get('/:slug', redirectLink)
 
-app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
-})
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+const errorHandler = require('./middlewares/errorHandler')
 /** 
  * * Toujours en dernier (gestion des erreurs)
 */
 app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
